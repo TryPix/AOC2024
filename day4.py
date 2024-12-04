176a5e5487dfe44bf4feee9804aa606bf4847272
Saimaneesh <yeturu.saimaneesh@gmail.com>
2024-12-04 07:12:48 +0100

    day4

--- day4.py
+++ day4.py
@@ -12,27 +12,30 @@ lines = text.split('\n')[:-1]
 ans = 0
 ans2 = 0
 
-for i in range(len(lines)):
-    for j in range(len(lines[i])):
+rows = len(lines)
+cols = len(lines[0])
 
-        if (i < len(lines)-2 and j < len(lines[i])-2):
+for i in range(rows):
+    for j in range(cols):
+
+        if (i < rows-2 and j < cols-2):
             if lines[i][j] + lines[i+1][j+1] + lines[i+2][j+2] in ('MAS', 'SAM'):
                 if lines[i][j+2] + lines[i+1][j+1] + lines[i+2][j] in ('MAS', 'SAM'):
                     ans2 += 1
 
-        if (j < len(lines[i])-3):
+        if (j < rows-3):
             if lines[i][j] + lines[i][j+1] + lines[i][j+2] + lines[i][j+3] in ('XMAS', 'SAMX'):
                 ans += 1
 
-        if (i < len(lines)-3):
+        if (i < rows-3):
             if lines[i][j] + lines[i+1][j] + lines[i+2][j] + lines[i+3][j] in ('XMAS', 'SAMX'):
                 ans += 1
         
-        if (i < len(lines)-3 and j < len(lines[i])-3):
+        if (i < rows-3 and j < cols-3):
             if lines[i][j] + lines[i+1][j+1] + lines[i+2][j+2] + lines[i+3][j+3] in ('XMAS', 'SAMX'):
                 ans += 1
         
-        if (i < len(lines)-3 and j >= 3):
+        if (i < rows-3 and j >= 3):
             if lines[i][j] + lines[i+1][j-1] + lines[i+2][j-2] + lines[i+3][j-3] in ('XMAS', 'SAMX'):
                 ans += 1
 
